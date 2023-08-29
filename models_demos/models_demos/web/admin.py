a4a012c70486efdca66c41b4c873d8ba2bfe7853
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-08-29 10:10:42 +0300

    Initial commit

--- models_demos/models_demos/web/admin.py
+++ models_demos/models_demos/web/admin.py
@@ -1,10 +1,59 @@
 from django.contrib import admin
 
-from models_demos.web.models import Employee
+from models_demos.web.models import Employee, NullBlankDemo, Department, Project, Category
 
 
 # Register your models here.
 # the Employeee model is enabled in django admin
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'first_name',
+                    'last_name', 'level',
+                    'department')
+    list_filter = ('level', 'first_name')
+
+    search_fields = ('first_name', 'last_name')
+
+    fieldsets = (
+        (
+            'Personal info',
+            {
+                'fields': ('first_name', 'last_name', 'age'),
+            }
+        ),
+        (
+            'Professional info',
+            {
+                'fields': ('level', 'years_of_experience'),
+            }
+        ),
+        (
+            'Company info',
+            {
+                'fields': ('department', 'is_full_time', 'email', 'start_date')
+            }
+        )
+    )
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(NullBlankDemo)
+class NullBlankDemoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
     pass
--- models_demos/models_demos/web/models.py
+++ models_demos/models_demos/web/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 # Model fields == class attributes in Model classes
 # Create your models here.
 
@@ -15,8 +14,38 @@ SQL Server: money
 '''
 
 
+class Department(models.Model):
+    name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f'Id: {self.pk}; Name: {self.name}'
+
+
+class Project(models.Model):
+    name = models.CharField(
+        max_length=30
+    )
+    code_name = models.CharField(
+        max_length=10,
+        unique=True,
+    )
+    deadline = models.DateField(
+
+    )
+
+    # additional info
+
 
 class Employee(models.Model):
+    LEVEL_JUNIOR = 'Junior'
+    LEVEL_REGULAR = 'Regular'
+    LEVEL_SENIOR = 'Senior'
+
+    LEVELS = (
+        (LEVEL_JUNIOR, LEVEL_JUNIOR),
+        (LEVEL_REGULAR, LEVEL_REGULAR),
+        (LEVEL_SENIOR, LEVEL_SENIOR),
+    )
     first_name = models.CharField(
         max_length=30,
     )
@@ -26,9 +55,18 @@ class Employee(models.Model):
     )
 
     level = models.CharField(
+        verbose_name='Seniority level',
         max_length=15,
+        choices=(
+            LEVELS
+            # ('jr','Junior'),
+            # ('reg','Regular'),
+            # ('sr','Senior')
+        )
     )
-    age = models.IntegerField()
+    age = models.IntegerField(
+        default=-1,
+    )
 
     years_of_experience = models.PositiveIntegerField()
     # text with unlimited length
@@ -36,28 +74,102 @@ class Employee(models.Model):
 
     start_date = models.DateTimeField()
     email = models.EmailField(
+        unique=True,
+        editable=True,
+    )
+
+    is_full_time = models.BooleanField(
         null=True
     )
 
-
     # automatically set on creation
     created_on = models.DateTimeField(
-        auto_now_add=True, # optional
+        auto_now_add=True,  # optional
     )
     # automatically set on each save/update
     updated_on = models.DateTimeField(
         auto_now=True,  # optional
 
     )
+    # One-to-many
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.RESTRICT,
+
+    )
+    # Many-to-many
+    projects = models.ManyToManyField(
+        Project,
+        related_name='employees',
+    )
+
     @property
     def fullname(self):
-        return  f'{self.first_name} {self.last_name}'
+        return f'{self.first_name} {self.last_name}'
+
     def __str__(self):
         return f'Id: {self.pk};Name: {self.fullname}'
 
 
+class AccessCard(models.Model):
+    employee = models.OneToOneField(
+        Employee,
+        on_delete=models.CASCADE,
+        primary_key=True
+    )
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=15
+    )
+
+    parent_category = models.ForeignKey(
+        'Category',
+        on_delete=models.RESTRICT,
+        null=True,
+        blank=True,
+
+    )
+
 
 '''
+
         Django-ORM 
 (Object-Relational-Mapping
 '''
+
+
+class EmployeesProjects(models.Model):
+    employee_id = models.ForeignKey(
+        Employee,
+        on_delete=models.RESTRICT)
+    project_id = models.ForeignKey(
+        Project,
+        on_delete=models.RESTRICT)
+
+    date_joined = models.DateField(
+        auto_now_add=True
+    )
+
+
+class NullBlankDemo(models.Model):
+    blank = models.IntegerField(
+        blank=True,
+        null=False,
+    )
+
+    null = models.IntegerField(
+        blank=False,
+        null=True,
+    )
+
+    blank_null = models.IntegerField(
+        blank=True,
+        null=True
+    )
+
+    default = models.IntegerField(
+        blank=False,
+        null=False,
+    )
